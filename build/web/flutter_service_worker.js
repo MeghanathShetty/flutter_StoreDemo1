577f636a071f3209bede8734350348497d1fd52f
Meghanath Shetty <127648939+MeghanathShetty@users.noreply.github.com>
2025-01-27 10:40:25 +0530

    fixed scrollbar appearing

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -23,14 +23,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
-"flutter_bootstrap.js": "60c013fc05c93e8f1bcaf0d0e242b4cd",
+"flutter_bootstrap.js": "144b7ba5fd8abf62c1ab2da112e64fb1",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "14a57828979103b0dd71dde59672f2fd",
 "/": "14a57828979103b0dd71dde59672f2fd",
-"main.dart.js": "9fbfd6dc6541a546655cedfdd809d1bb",
+"main.dart.js": "0c124de909cffdffe9d6b74bbef17df3",
 "manifest.json": "b1384926daae69b947562af6003df3ac",
 "version.json": "ab4643da63ff2b0c4ac892e8be6f39e8"};
 // The application shell files that are downloaded before a service worker can
